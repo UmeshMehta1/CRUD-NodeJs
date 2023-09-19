8e1deb09cccefa360876293af99a01b61423344a
Umesh Kumar Mehta <120069900+UmeshMehta1@users.noreply.github.com>
2023-09-19 19:41:12 +0545

    Coorie

--- Cookies/controller/studentController.js
+++ Cookies/controller/studentController.js
@@ -1,11 +1,19 @@
 class StudentController{
     static set_cookie = (req,res)=>{
+        res.cookie("username", "umeshmehta1", {maxAge: 40000});
+        res.cookie("username2", "umeshmehta2");
+        res.cookie("name", "umeshmehta3");
         res.send("set cokies.......")
     }
      static get_cookie = (req,res)=>{
+        console.log(req.cookies)
+        console.log(req.cookies.username2)
+        console.log(req.cookies.name)
         res.send("get cokies.......")
     }
      static delete_cookie = (req,res)=>{
+        res.clearCookie("username")
+        res.clearCookie("name")
         res.send("delete cokies.......")
     }
     
